40150e9fb9a15f6678c8a1eb666f1413476e3cf9
Nathan Mayhook <jnmayhook@gmail.com>
2022-04-30 23:38:10 -0700

    first draft

---
+++ assets/js/scriptHighscores.js
@@ -0,0 +1,10 @@
+var intials = document.getElementById("initials")
+var enterInitialsField = document.getElementById("enterInitialsField")
+var enterInitialsBtn = document.getElementById("enterInitialsBtn")
+var finalScore = document.getElementById("finalScore")
+var highscoreForm = document.getElementById("highscore-form")
+var highscoreList = document.getElementById("highscore-list")
+
+
+
+
